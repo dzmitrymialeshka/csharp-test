0026f711d298d6f8b6da17fa346e7b02b725b338
dzmitrymialeshka <110839344+dzmitrymialeshka@users.noreply.github.com>
2023-09-21 17:15:04 +0200

    Update calculator.cs
    beautified code

--- calculator.cs
+++ calculator.cs
@@ -8,36 +8,35 @@ public class Calculator
         int operand2 = 0;
         Console.Write("Please enter your sign: ");
         string sign = Console.ReadLine();
-        
+
         switch (sign)
         {
-        case "+":
-            Console.Write(operand1 + operand2);
-        break;
-        
-        case "-":
-            Console.Write(operand1 - operand2);
-        break; 
-        
-        case "*":
-            Console.Write(operand1 * operand2);
-        break; 
-        
-        case "/":
-            if (operand1 == 0)
-            {
-            Console.WriteLine("Attempt to divide by zero");
-            }
-            else if (operand2 == 0)
-            {
-            Console.WriteLine("Attempt to divide by zero");   
-            }
-            else
-            {
-            Console.Write(operand1 / operand2);
-            }
-        break;
-        
+            case "+":
+                Console.Write(operand1 + operand2);
+                break;
+
+            case "-":
+                Console.Write(operand1 - operand2);
+                break;
+
+            case "*":
+                Console.Write(operand1 * operand2);
+                break;
+
+            case "/":
+                if (operand1 == 0)
+                {
+                    Console.WriteLine("Attempt to divide by zero");
+                }
+                else if (operand2 == 0)
+                {
+                    Console.WriteLine("Attempt to divide by zero");
+                }
+                else
+                {
+                    Console.Write(operand1 / operand2);
+                }
+                break;
         }
     }
 }
